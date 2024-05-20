079b16ca7ddc9f15e5b8d28a4936a13bce613a5f
CwGmZ971 <callumwhite2ndacc@gmail.com>
2024-05-20 20:38:55 +0100

    update install.bat

--- exe_version/install.bat
+++ exe_version/install.bat
@@ -8,6 +8,6 @@ rem Create the installation directory if it doesn't exist
 if not exist "%INSTALL_DIR%" mkdir "%INSTALL_DIR%"
 
 rem Extract the contents of the zip file to the installation directory
-powershell Expand-Archive -Path "YourArchive.zip" -DestinationPath "%INSTALL_DIR%"
+powershell Expand-Archive -Path "IP_Lookup_Utility.zip" -DestinationPath "%INSTALL_DIR%"
 
 echo Installation completed.
